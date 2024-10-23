5ff72346f0bc5bb7f1b77f63a8672081e522a5d2
RockingWithTheBest <sikanweclyde@gmail.com>
2024-10-23 11:43:42 +0300

    feat: added photos for the BodyTwoCompoenent

---
+++ Real Estate Site/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import Header from './Components/Header/Header'
+import BodYOne  from './Components/BodyOne/BodyOne';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Header/>
+        <BodYOne/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ Real Estate Site/src/Components/BodyOne/BodyOne.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ColorToggleButton from '../ToggleButton/ColorToggleButton';
+
+const BodYOne =() => {
+    return(
+        <div className='BodyOne'>
+            <div>
+                </div>
+                    <span>Buy, rent, or sell your property easily</span>
+                    <p>A great platform to buy, sell, or even rent your properties without any commisions.</p>
+                <div>
+                <div>
+                    <ColorToggleButton/>
+                </div>
+                <div></div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default BodYOne;
